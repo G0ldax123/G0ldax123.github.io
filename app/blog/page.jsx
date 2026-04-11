61798c5e986045785486c148db7cdcd558c62416
G0ldax123 <ax290803@gmail.com>
2026-04-11 15:53:22 +0900

    Update blog page heading

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,7 +1,7 @@
 ﻿const Page = () => {
   return (
     <div>
-      <h1>{'\uBE14\uB85C\uADF8 / \uAE30\uC220\uBB38\uC11C'}</h1>
+      <h1>{'\uBE14\uB85C\uADF8/\uAE30\uC220\uBB38\uC11C'}</h1>
       <p>{'\uBE14\uB85C\uADF8/\uAE30\uC220\uBB38\uC11C \uAD00\uB828 \uB0B4\uC6A9 \uC815\uB9AC \uC608\uC815\uC785\uB2C8\uB2E4.'}</p>
     </div>
   );
